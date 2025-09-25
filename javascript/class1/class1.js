4fe38e81a73516225fb7734bdcf11059901ef495
rishabh-32700 <rishabh.sharma@blackngreen.com>
2025-09-25 22:47:04 +0530

    functions

--- javascript/class1/class1.js
+++ javascript/class1/class1.js
@@ -1,10 +1,75 @@
-function hello() {
-  console.log("Hello !!!");
+// function 
 
-  return function bye(){
-    console.log("nikl jaaaa !");
+// y = hello(x)
+
+
+// parameterized 
+
+
+// non parameterized
+// func dec
+// console.log(hello());
+// function hello() {
+//   console.log("hello");
+// }
+// console.log(hello());
+
+// // named func exp
+// console.log(hey);
+// var i = 0
+// var hey = function cuvette() {
+//   i++
+//   if(i<10){
+//     hey()
+//     console.log(i);
+//   }else{
+//     console.log("hello");
+//   }
+// }
+// hey()
+// cuvette()
+// console.log(hey());
+
+
+// // Anno funct
+// (function() {
+//   console.log("hello");
+// })
+
+
+
+// // func exp
+// const hey2 = function() {
+//   console.log("hello");
+// }
+
+// // arrow 
+// const hey3 = () => {
+//   console.log("hello");
+// }
+
+// // IIFE
+// (function() {
+//   console.log("hello");
+// })()
+
+// // async
+// async function name() {
+  
+// }
+
+// const hey4 = async () => {
+//   console.log("hello");
+// }
+
+
+// for(const i =0; i<=10; i++){
+//   console.log(i);
+// }
+
+(function() {
+  console.log("hello");
+  return function hello(){
+    console.log("kya haal hai????");
   }
-}
-
-var a = hello()
-console.log(a());
+})()()()
